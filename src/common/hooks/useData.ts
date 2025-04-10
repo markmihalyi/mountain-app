6cbfa5cc5c42be756021dcc1ebc5090a143cb7ce
markmihalyi <contact@markmihalyi.hu>
2025-04-10 15:35:01 +0200

    fix hook's name

--- src/common/hooks/useData.ts
+++ src/common/hooks/useData.ts
@@ -2,8 +2,8 @@ import { useContext } from "preact/hooks";
 import DataContext from "../providers/DataProvider/index.js";
 import { IDataContext } from "../providers/DataProvider/types.js";
 
-const useAuth = () => {
+const useData = () => {
   return useContext<IDataContext>(DataContext);
 };
 
-export default useAuth;
+export default useData;
